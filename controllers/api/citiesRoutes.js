6d13e5b93f214660ddbd9e5890ed48d075787e83
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-03-27 20:39:33 -0500

    adjust withAuth in cities and entry routes

--- controllers/api/citiesRoutes.js
+++ controllers/api/citiesRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { City, Comment, Rating } = require("../../models");
-const withAuth = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
   // If the user is already logged in, redirect the request to another route
@@ -23,18 +22,5 @@ router.get("/", async (req, res) => {
   res.status(500).json(err);
 }
 });
-
-router.post("/", withAuth, async (req, res) => {
-    try {
-      const newCity = await City.create({
-              ...req.body,
-              user_id: req.session.user_id,
-            });
-  
-      res.status(200).json(newCity);
-  } catch (err) {
-    res.status(400).json(err);
-    }
-  });
 
   module.exports = router;
--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const { City, Comment, Rating } = require("../../models");
-// const withAuth = require("../utils/auth");
-
+const withAuth = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
     try {
@@ -15,10 +14,7 @@ router.get("/", async (req, res) => {
           }
         ]
       });
-  
-      // Serialize data so the template can read it
       const cities = cityData.map((city) => city.get({ plain: true }));
-      // homepage reference handlebars
       res.render("entry", {
         cities
       });
@@ -28,26 +24,13 @@ router.get("/", async (req, res) => {
     }
   });
 
-//   router.post('/', async (req, res) => {
-//   try {
-//     const newComment = await Comment.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
-
-//     res.status(200).json(newComment);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
       const newRating = await Rating.create({
         ...req.body
       });
   
-      res.status(200).json(newComment);
+      res.status(200).json(newRating);
     } catch (err) {
       res.status(400).json(err);
     }
