f0c0d5929593ac278964262dd2d427d8203e0488
kduesler <kyle.duesler.09@gmail.com>
2023-03-28 19:27:37 -0500

    start city route

--- controllers/api/citiesRoutes.js
+++ controllers/api/citiesRoutes.js
@@ -2,18 +2,38 @@ const router = require("express").Router();
 const { City, Comment, Rating } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.get("/", async (req, res) => {
+router.get("/:id", async (req, res) => {
   // If the user is already logged in, redirect the request to another route
   try{
     
-    const cityData = await City.findAll({
+    const cityData = await City.findByPk(req.params.id, {
       include: [
         {
           model: Comment,
+        },
+        {
+          model: Rating
         }
       ]
     });
-    const cities = cityData.map((city) => city.get({ plain: true }));
+    const cities = cityData.get({ plain: true });
+    
+    // Pass serialized data and session flag into template
+    for (let i = 0; i < cities.length; i++) {
+      const city = cities[i];
+      const totalRatings = {
+        //...city.ratings[0]
+        average_rating: 0,
+      };
+      for (let j = 0; j < city.ratings.length; j++) {
+        const rating = city.ratings[j];
+        totalRatings.average_rating += parseFloat(rating.average_rating);
+      }
+      totalRatings.average_rating = (totalRatings.average_rating/city.ratings.length).toFixed(1)
+      city.totalRatings = totalRatings;
+      // push updates to newCities
+    }
+    
     res.render("cityname", {
       cities
     });
